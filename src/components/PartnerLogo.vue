d7f0ebf882723de1f33c5af48a4cfa223020cc70
Ryan Adhitama Putra <ryanadhitama2@gmail.com>
2020-03-17 16:39:44 +0800

    first commit

---
+++ src/components/PartnerLogo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="partner-logo">
+        <div class="container">
+            <carousel class="logo-carousel" :autoplay="true" :items="5" :loop="true" :dots="false" :nav="false">
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-1.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-2.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-3.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-4.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-5.png" alt="" />
+                    </div>
+                </div>
+            </carousel>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    import carousel from 'vue-owl-carousel'
+    export default{
+        name : "PartnerLogo",
+        components : {
+            carousel
+        }
+    }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <!-- Header Section Begin -->
+    <Header/>
+    <!-- Header End -->
+
+    <!-- Carousel Section Begin -->
+    <BannerCarousel/>
+    <!-- Carousel Section End -->
+
+    <!-- Carousel Section Begin -->
+    <ProductCarousel/>
+    <!-- Carousel Section End -->
+
+    <!-- InstaImage Section Begin -->
+    <InstaImage/>
+    <!-- InstaImage Section End -->
+
+    <!-- PartnerLogo Section Begin -->
+    <PartnerLogo/>
+    <!-- PartnerLogo Section End -->
+
+    <!-- Footer Section Begin -->
+    <Footer />
+    <!-- Footer Section End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import BannerCarousel from '@/components/BannerCarousel.vue'
+import ProductCarousel from '@/components/ProductCarousel.vue'
+import InstaImage from '@/components/InstaImage.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    BannerCarousel,
+    ProductCarousel,
+    InstaImage,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
